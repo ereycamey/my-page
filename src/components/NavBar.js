4941ab638eaebfb8c96babd8958fb2243cd324e3
ereycamey <deveraericamae@gmail.com>
2023-01-24 14:50:31 +0800

    table in users

---
+++ src/components/NavBar.js
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import { FaLock, FaHome, FaList, FaUsers } from "react-icons/fa";
+import { MdGroups, MdPerson } from "react-icons/md";
+import "../Styles/main.css";
+import voting from './voting.png'
+import { __DATA__ } from "./data";
+import {
+  MainContainer,
+  Container,
+  BarChartContainer,
+  Number,
+  BlackLine,
+  MakeBar
+} from "./styles";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="page">
+        <header>
+          <h3><img src={voting} height="40" width="40" style={{ position: 'relative' }} alt="login" /></h3>
+          <Link to="navbar"><button className="button"><FaHome />  Overview</button></Link>
+          <Link to="votinglist"><button className="button"><FaList />  Ballot</button></Link>
+          <Link to="users"><button className="button"><FaUsers />  Voters</button></Link>
+          <Link to="/loginform"><button className="nav-btn">
+            <FaLock />
+          </button>
+          </Link>
+        </header>
+        <br></br>
+        <br></br>
+        <div className="p1">&nbsp; &nbsp; &nbsp; &nbsp;<FaHome />  Overview</div>
+        <br></br>
+        <center>
+        <Link to="voters"><button className="button1"><MdGroups />  Voters   &nbsp; &nbsp; &nbsp; &nbsp;     150</button></Link>
+        <Link to="voted"><button className="button1"><MdPerson />  Voted   &nbsp; &nbsp; &nbsp; &nbsp;     101</button></Link>
+        </center>
+        <Container>
+          <MainContainer>
+            {__DATA__.map(({ distance, colors }, i) => {
+              return (
+                <BarChartContainer key={i}>
+                  <Number color={colors[1]}>{distance} Votes</Number>
+                  <MakeBar height={distance * 2} colors={colors} />
+                </BarChartContainer>
+              );
+            })}
+          </MainContainer>
+          <BlackLine />
+        </Container>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
